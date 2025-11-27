2aa9f0a28ac9abff13d894a37e97f04ca1472d1f
Student-2024 <student-2024@ashalim.org>
2025-11-27 14:31:32 +0200

    27/11/25 2

--- app/src/main/java/com/example/pr/Information_page.java
+++ app/src/main/java/com/example/pr/Information_page.java
@@ -1,8 +1,6 @@
 package com.example.pr;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,35 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class HomePage extends AppCompatActivity {
+public class Information_page extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_home_page);
+        setContentView(R.layout.activity_information_page);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-
-    public void onClicksignUp(View view)
-    {
-        Intent go= new Intent(HomePage.this, SignUp.class);
-        startActivity(go);
-    }
-
-    public void onClickLogIn(View view)
-    {
-        Intent go= new Intent(HomePage.this, LogIn.class);
-        startActivity(go);
-    }
-
-    public void onClickAudot(View view)
-    {
-        Intent go= new Intent(HomePage.this, Information_page.class);
-        startActivity(go);
     }
 }
